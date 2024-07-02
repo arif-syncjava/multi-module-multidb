c648515971a4d7f5e5d0087c9324e370dcfe2093
MD ARIFUL ISLAM <arif.syncjava@gmail.com>
2024-07-02 14:58:19 +0600

    multi-module and multi dataSource initial setup

---
+++ database1/src/main/java/com/arifsyncjava/database1/data/DataEntry.java
@@ -0,0 +1,12 @@
+package com.arifsyncjava.database1.data;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataEntry implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
